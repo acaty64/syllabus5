2d70ae3c10f47db6e36d84a8ed29afde8fadc9d6
vagrant <vagrant@homestead>
2018-09-10 23:52:34 +0000

    root CI + phpunit.xml.ci

--- ci/setup_phpunit.sh
+++ ci/setup_phpunit.sh
@@ -4,15 +4,15 @@
     
     phpenv local 7.1
 
-    mysql -e 'create database ucss_tests;'
+    mysql -e 'create database syllabus_tests;'
 
     cp .env.codeship .env
     
     composer install --prefer-dist --no-interaction -o --optimize-autoloader
     
     php artisan key:generate    
-    php artisan migrate --force --env="ucss_tests"
-    php artisan db:seed --env="ucss_tests"
+    php artisan migrate --force --env="syllabus_tests"
+    php artisan db:seed --env="syllabus_tests"
 
     cp phpunit.xml.ci phpunit.xml
 
